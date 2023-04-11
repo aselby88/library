d5b83adc696ad4e854fa2daa2f9015ba856e66a5
April Selby <a.selby88@gmail.com>
2023-04-11 10:43:37 -0400

    feat: tidy

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# library
-Kotlin examples
+# Library
+
+Starting point for kotlin workshop.
+
+Introducing kotlin features:
+- Data classes
+- Null safety
+- Smart casting
+- Extension functions
--- src/main/kotlin/org/library/Main.kt
+++ src/main/kotlin/org/library/Main.kt
@@ -1,15 +1,13 @@
 package main.kotlin.org.library
 
 import main.kotlin.org.library.domain.Book
-import main.kotlin.org.library.domain.getRating
 
 fun main(args: Array<String>) {
-    val harryPotter1 = Book("Harry Potter and the Philosopher's Stone", "JK Rowling", null)
+    val harryPotter1 = Book("Harry Potter and the Philosopher's Stone", "JK Rowling")
     println(harryPotter1)
     println(harryPotter1.title)
     harryPotter1.author = "April"
     println(harryPotter1)
-    println(harryPotter1.getRating())
 
     val harryPotter2 = harryPotter1.copy()
     val equals = harryPotter1.equals(harryPotter2)
